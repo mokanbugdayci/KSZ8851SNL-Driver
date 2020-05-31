71b1c46d4e3f99f8e4b446c401ec951bc0b8bbfd
Mahmut Okan Buğdaycı <mokanbugdayci@gmail.com>
2020-05-31 20:33:41 +0300

    Licence header

--- ksz8851snl/inc/ksz8851.h
+++ ksz8851snl/inc/ksz8851.h
@@ -6,7 +6,17 @@
  * @copyright   : 	GNU licence.
  * @date        : 	01.05.2020
  * @revision	: 	v.1.0.0 - Driver files created
+ 
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/
+ 
  ******************************************************************************/
+ 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __KSZ8851_H
 #define __KSZ8851_H
